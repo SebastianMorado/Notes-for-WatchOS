6a936eec7f89f4e1d8adceff4f5beebc57f6a82d
SebastianMorado <sabby.morado@gmail.com>
2022-01-18 21:29:42 +0800

    Initial Commit

---
+++ NotesWatchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesWatchOS WatchKit Extension
+//
+//  Created by Sebastian Morado on 1/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotesWatchOS WatchKit Extension/NotesWatchOSApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesWatchOSApp.swift
+//  NotesWatchOS WatchKit Extension
+//
+//  Created by Sebastian Morado on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesWatchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
